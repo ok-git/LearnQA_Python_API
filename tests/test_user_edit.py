b00392abd5c2fecd32e5e21107dfb199f07f9467
ok-git <okmail.inbox@gmail.com>
2021-11-27 16:13:09 +0700

    Creating framework - MyRequests

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,13 +1,12 @@
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-
+from lib.my_requests import MyRequests
 
 class TestUserEdit(BaseCase):
     def test_edit_just_creatde_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
-        response1 = requests.post('https://playground.learnqa.ru/api/user', data=register_data)
+        response1 = MyRequests.post('/user', data=register_data)
 
         Assertions.assert_status_code(response1, 200)
         Assertions.assert_json_has_key(response1, 'id')
@@ -22,7 +21,7 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post('https://playground.learnqa.ru/api/user/login', data=login_data)
+        response2 = MyRequests.post('/user/login', data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
@@ -30,17 +29,17 @@ class TestUserEdit(BaseCase):
         # EDIT
 
         new_name = 'Changed Name'
-        response3 = requests.put(f'https://playground.learnqa.ru/api/user/{user_id}',
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid},
-                                 data={'firstName': new_name})
+        response3 = MyRequests.put(f'/user/{user_id}',
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={'firstName': new_name})
 
         Assertions.assert_status_code(response3, 200)
 
         # GET
-        response4 = requests.get(f'https://playground.learnqa.ru/api/user/{user_id}',
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid})
+        response4 = MyRequests.get(f'/user/{user_id}',
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
 
         Assertions.assert_json_value_by_name(
             response4,
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -8,7 +8,7 @@ class TestUserRegister(BaseCase):
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
-        response = requests.post('https://playground.learnqa.ru/api/user', data=data)
+        response = MyRequests.post('/user', data=data)
 
         Assertions.assert_status_code(response, 200)
         Assertions.assert_json_has_key(response, 'id')
@@ -17,7 +17,7 @@ class TestUserRegister(BaseCase):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
 
-        response = requests.post('https://playground.learnqa.ru/api/user', data=data)
+        response = MyRequests.post('/user', data=data)
 
         Assertions.assert_status_code(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists",\
