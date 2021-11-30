6cebf060607381731e6d0043a2caa4005ed5f943
ok-git <okmail.inbox@gmail.com>
2021-11-30 23:12:07 +0700

    Ex. 16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -30,3 +30,36 @@ class TestUserGet(BaseCase):
 
         expected_fields = ['username', 'email', 'firstName', 'lastName']
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_another_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post('/user', data=register_data)
+
+        Assertions.assert_status_code(response1, 200)
+        Assertions.assert_json_has_key(response1, 'id')
+
+        email = register_data['email']
+        password = register_data['password']
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post('/user/login', data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # GET user_id 2
+        response3 = MyRequests.get('/user/2',
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_json_has_key(response3, 'username')
+        Assertions.assert_json_has_not_key(response3, 'email')
+        Assertions.assert_json_has_not_key(response3, 'firstName')
+        Assertions.assert_json_has_not_key(response3, 'lastName')
+
+
